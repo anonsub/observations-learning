550faf6e7c08cae93b472c52eea9370caceae6fa
anonsub <anon@sub.com>
2018-01-23 16:58:58 +0100

    Updated experiments scripts

--- experiments/ijcai18/generate_table_3.sh
+++ experiments/ijcai18/generate_table_3.sh
@@ -6,9 +6,9 @@ ulimit -t 1000
 for i in "${arr[@]}"
 do
    echo $i
+   ../../src/compiler.py -s ../../benchmarks/ijcai18/training/$i/ empty_domain test plan 2
+   ../../src/testset-evaluator.py learned_domain.pddl ../../benchmarks/ijcai18/test/$i/ test plan 2
 
-   ../../src/compiler.py -s ../../benchmarks/ijcai18/observations/$i/ empty_domain test plan 2
 
-   ../../src/validator.py learned_domain.pddl ../../benchmarks/ijcai18/test/$i/ test plan 2
-
+   echo 
 done
